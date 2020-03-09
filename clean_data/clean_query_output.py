cfbf0c7042a6b2317d9692e4b7d66e7da0d11ee5
yiyi chen <yiyi.chen@trustyou.com>
2020-03-09 23:07:48 +0100

    solve conflicts

--- clean_data/clean_query_output.py
+++ clean_data/clean_query_output.py
@@ -63,10 +63,8 @@ def clean_query_outputs(filename):
 
     print(new_content_list)
 
-
-
 if __name__ == '__main__':
-    clean_query_outputs("../query_outputs_all/77a35d4e-0d61-4390-ae86-5f8cf2bb9080")
+    clean_query_outputs("query_outputs_all/77a35d4e-0d61-4390-ae86-5f8cf2bb9080")
 
 
 
