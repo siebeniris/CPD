b9163f55605c466b803a890e8da841fc5540330f
Yiyi Chen <chen.yiyi@protonmail.com>
2020-04-21 00:07:21 +0200

    bert sentiment analysis

---
+++ topic_modeling/bert_sentiment_analysis.py
@@ -0,0 +1,11 @@
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import tensorflow as tf
+import tensorflow_hub as hub
+from datetime import datetime
+
+import bert
+from bert import run_classifier
+from bert import optimization
+from bert import tokenization
+
